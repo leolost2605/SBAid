595dbbc8596d81918677619959f1a2534cf98f15
Leonhard Kargl <leo.kargl@proton.me>
2025-07-09 14:17:36 +0200

    Add lint manual

--- sbaid/__init__.py
+++ sbaid/__init__.py
@@ -1,3 +1,7 @@
+"""
+SBAid yay.
+"""
+
 import sys
 
 # import gi
@@ -11,24 +15,26 @@ import sys
 
 # class MyApplication(Adw.Application):
 #     def __init__(self):
-#         super().__init__(application_id="io.github.leolost2605.SBAid")
-#         GLib.set_application_name('SBAid')
+#         super().__init__(application_id="io.github.leolost2605.sbaid")
+#         GLib.set_application_name('sbaid')
 #
 #     def do_activate(self):
 #         shumate_map = Shumate.SimpleMap()
 #         shumate_map.set_vexpand(True)
-#         shumate_map.set_map_source(Shumate.RasterRenderer.new_from_url("https://tile.openstreetmap.org/{z}/{x}/{y}.png"))
+#         shumate_map.set_map_source(Shumate.RasterRenderer.new_from_ur
+#         l("https://tile.openstreetmap.org/{z}/{x}/{y}.png"))
 #
 #         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10, vexpand=True)
 #         box.append(Adw.HeaderBar())
 #         box.append(shumate_map)
 #
-#         window = Adw.ApplicationWindow(application=self, title="SBAid")
+#         window = Adw.ApplicationWindow(application=self, title="sbaid")
 #         window.set_content(box)
 #         window.present()
 
 
 # app = MyApplication()
 # exit_status = app.run(sys.argv)
-print("Hello World")
+my_text: str = "hello world"
+print (my_text)
 sys.exit(0)
