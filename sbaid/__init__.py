dde378ca0ed55a966ff08e467b233a3f9d2a619c
Leonhard Kargl <leo.kargl@proton.me>
2025-07-09 14:47:32 +0200

    Fix code style hopefully

--- sbaid/__init__.py
+++ sbaid/__init__.py
@@ -1,38 +1,7 @@
 """
-SBAid yay.
+The main SBAid package. It is the entry point into the application
+and contains only the application and the other packages.
 """
 
-import sys
-
-import gi
-
-gi.require_version("Gtk", "4.0")
-gi.require_version("Adw", "1")
-gi.require_version("Shumate", "1")
-from gi.repository import GLib, Gtk
-from gi.repository import Adw
-from gi.repository import Shumate
-
-class MyApplication(Adw.Application):
-    def __init__(self):
-        super().__init__(application_id="io.github.leolost2605.sbaid")
-        GLib.set_application_name('sbaid')
-
-    def do_activate(self):
-        shumate_map = Shumate.SimpleMap()
-        shumate_map.set_vexpand(True)
-        shumate_map.set_map_source(Shumate.RasterRenderer.new_from_url("https://tile.openstreetmap.org/{z}/{x}/{y}.png"))
-
-        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10, vexpand=True)
-        box.append(Adw.HeaderBar())
-        box.append(shumate_map)
-
-        window = Adw.ApplicationWindow(application=self, title="sbaid")
-        window.set_content(box)
-        window.present()
-
-app = MyApplication()
-exit_status = app.run(sys.argv)
-my_text: str = "hello world"
-print (my_text)
-sys.exit(0)
+if __name__ == '__main__':
+    print("Hello world!")
--- sbaid/model/__init__.py
+++ sbaid/model/__init__.py
@@ -1,2 +1,5 @@
-def myfunc():
-    print("Hello World")
+"""
+The model package contains the model of SBAid.
+It defines the interfaces for the algorithms and simulators and
+contains the PTV Vissim and CSV Dummy simulator implementation.
+"""
