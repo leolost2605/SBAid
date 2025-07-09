d2d8e725a86821dac5dde4441cd12a9054e1ef29
Leonhard Kargl <leo.kargl@proton.me>
2025-07-09 13:41:08 +0200

    Add another linter

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import gi
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
+gi.require_version("Shumate", "1")
 from gi.repository import GLib, Gtk
 from gi.repository import Adw
 from gi.repository import Shumate
