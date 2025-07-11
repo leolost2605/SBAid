7c911fbcc3a455de7c7294fc27525d3bf40814da
Louis <uqyrq@student.kit.edu>
2025-07-11 18:51:34 +0200

    fix 'name' to 'type' in properties

--- sbaid/common/diagram_type.py
+++ sbaid/common/diagram_type.py
@@ -4,8 +4,8 @@ from gi.repository import GObject
 
 class DiagramType(GObject.GObject):
     """TODO"""
-    diagram_type_id = GObject.Property(name=str, flags=GObject.PARAM_READABLE)
-    name = GObject.Property(name=str, flags=GObject.PARAM_WRITABLE)
+    diagram_type_id = GObject.Property(type=str, flags=GObject.PARAM_READABLE)
+    name = GObject.Property(type=str, flags=GObject.PARAM_WRITABLE)
 
     def __init__(self, diagram_type_id: str, name: str) -> None:
         """Initialize the diagram type."""
--- sbaid/common/simulator_type.py
+++ sbaid/common/simulator_type.py
@@ -4,9 +4,9 @@ from gi.repository import GObject
 
 class SimulatorType(GObject.GObject):
     """TODO"""
-    simulator_type_id = GObject.Property(name=str,
+    simulator_type_id = GObject.Property(type=str,
                                          flags=GObject.PARAM_READABLE)
-    name = GObject.Property(name=str, flags=GObject.PARAM_READWRITE)
+    name = GObject.Property(type=str, flags=GObject.PARAM_READWRITE)
 
     def __init__(self, simulator_type_id: str, name: str) -> None:
         """Initialize the simulator type with an id and a name."""
