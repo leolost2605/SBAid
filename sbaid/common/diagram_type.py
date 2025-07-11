11f359066033d5157451d23b44518b96afd292d8
Louis <uqyrq@student.kit.edu>
2025-07-11 15:26:58 +0200

    add property functionality

--- sbaid/common/diagram_type.py
+++ sbaid/common/diagram_type.py
@@ -1,12 +1,14 @@
 """This module defines the DiagramType class"""
+from gi.repository import GObject
 
-
-class DiagramType:
-    """This class represents a type of diagram.
-    Attributes:
-        diagram_type_id (str): The unique identifier for the diagram type.
-        name (str): The name of the diagram type.
+class DiagramType(GObject.GObject):
     """
+    This class represents a type of diagram.
+    """
+    diagram_type_id = GObject.Property(name=str)
+    name = GObject.Property(name=str)
+
     def __init__(self, diagram_type_id: str, name: str) -> None:
+        """Initialize the diagram type."""
         self.diagram_type_id = diagram_type_id
         self.name = name
--- sbaid/common/simulator_type.py
+++ sbaid/common/simulator_type.py
@@ -1,12 +1,14 @@
 """this module defines the SimulatorType class"""
+from gi.repository import GObject
 
-
-class SimulatorType:
+class SimulatorType(GObject.GObject):
     """This class represents a type of simulator.
     Attributes:
         simulator_type_id (str): The unique identifier for the simulator type.
         name (str): The name of the simulator type.
     """
+    simulator_type_id = GObject.Property(name=str)
+    name = GObject.Property(name=str)
 
     def __init__(self, simulator_type_id: str, name: str) -> None:
         """Initialize the simulator type with an id and a name."""
