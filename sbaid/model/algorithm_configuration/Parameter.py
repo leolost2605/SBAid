54a06f611b17c16fed22f75d03ae14491927d1c2
deniz karabati <daphne41.17@gmail.com>
2025-07-13 18:59:34 +0200

    initial commit, very unfinished, just started

---
+++ sbaid/model/algorithm_configuration/Parameter.py
@@ -0,0 +1,15 @@
+"""this module defines the Parameter class."""
+class Parameter:
+
+    def __init__(self, parameter_name, parameter_value):
+    def add_tag(self, tag: Tag) -> None:
+        """todo"""
+        pass
+
+    def remove_tag(self, tag: Tag) -> None:
+        """todo"""
+        pass
+
+    def load_from_db(self) -> None:
+        """todo"""
+        pass
