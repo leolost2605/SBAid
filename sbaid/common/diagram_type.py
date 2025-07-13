78b1dfb86ed4eba64b3659f34a174104a8962cb4
Louis Fuchs <152591363+briorl@users.noreply.github.com>
2025-07-13 19:16:54 +0200

    add common skeleton

---
+++ sbaid/common/diagram_type.py
@@ -0,0 +1,20 @@
+"""This module defines the DiagramType class"""
+from gi.repository import GObject
+
+
+class DiagramType(GObject.GObject):
+    """TODO"""
+    diagram_type_id = GObject.Property(
+        type=str,
+        flags=GObject.ParamFlags.READABLE |
+        GObject.ParamFlags.WRITABLE |
+        GObject.ParamFlags.CONSTRUCT_ONLY)
+    name = GObject.Property(
+        type=str,
+        flags=GObject.ParamFlags.READABLE |
+        GObject.ParamFlags.WRITABLE |
+        GObject.ParamFlags.CONSTRUCT)
+
+    def __init__(self, diagram_type_id: str, name: str) -> None:
+        """TODO"""
+        super().__init__(diagram_type_id=diagram_type_id, name=name)
