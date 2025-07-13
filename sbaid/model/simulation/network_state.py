2d2209c95d1a9cc4618cdce2fae6c4f2ea380fc1
Leonhard Kargl <leo.kargl@proton.me>
2025-07-13 23:02:23 +0200

    fix lists

--- sbaid/model/simulation/network_state.py
+++ sbaid/model/simulation/network_state.py
@@ -1,5 +1,5 @@
 """TODO"""
-from gi.repository import GObject
+from gi.repository import Gio, GObject
 from sbaid.model.simulation.cross_section_state import CrossSectionState
 from sbaid.common.coordinate import Coordinate
 
@@ -7,14 +7,16 @@ from sbaid.common.coordinate import Coordinate
 class NetworkState(GObject.GObject):
     """TODO"""
     route = GObject.Property(
-        type=list,
+        type=Gio.ListModel,
         flags=GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE |
         GObject.ParamFlags.CONSTRUCT_ONLY)
     cross_section_states = GObject.Property(
-        type=list,
+        type=Gio.ListModel,
         flags=GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE |
         GObject.ParamFlags.CONSTRUCT_ONLY)
 
     def __init__(self, route: list[Coordinate],
                  cross_section_states: list[CrossSectionState]) -> None:
-        super().__init__(route=route, cross_section_states=cross_section_states)
+        """TODO"""
+        # TODO: convert lists to listmodels aka stores
+        super().__init__()
