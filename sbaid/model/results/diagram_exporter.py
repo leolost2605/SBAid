51c9d9ee8a34b54ae7ab13d395fdc3eefb11b41f
leonormsimoes <142237594+leonormsimoes@users.noreply.github.com>
2025-07-14 10:24:32 +0200

    results package skeleton (#10)

---
+++ sbaid/model/results/diagram_exporter.py
@@ -0,0 +1,20 @@
+"""todo"""
+
+from gi.repository import Gio, GObject
+from sbaid.model.results.result import Result
+from sbaid.common.image_format import ImageFormat
+from sbaid.common.diagram_type import DiagramType
+
+
+class DiagramExporter(GObject.GObject):
+    """todo"""
+
+    # GObject.Property definitions
+    available_diagram_types = GObject.Property(type=Gio.ListModel,
+                                               flags=GObject.ParamFlags.READABLE |
+                                               GObject.ParamFlags.WRITABLE |
+                                               GObject.ParamFlags.CONSTRUCT_ONLY)
+
+    def create_diagram(self, result: Result, cross_section_ids: list[str],
+                       image_format: ImageFormat, diagram_type: DiagramType) -> None:
+        """todo"""
---
+++ sbaid/model/results/heatmap_generator.py
@@ -0,0 +1,7 @@
+"""todo"""
+
+from sbaid.model.results.global_diagram_generator import GlobalDiagramGenerator
+
+
+class HeatmapGenerator(GlobalDiagramGenerator):
+    """todo"""
