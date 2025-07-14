9717d37255fa24f7756caf75340c07c02a6f370c
leonormsimoes <142237594+leonormsimoes@users.noreply.github.com>
2025-07-14 18:08:41 +0200

    use correct network import

--- sbaid/model/algorithm/algorithm.py
+++ sbaid/model/algorithm/algorithm.py
@@ -4,7 +4,7 @@ from gi.repository import Gio, GObject
 from sbaid.model.simulation.parameter_configuration_state import ParameterConfigurationState
 from sbaid.model.simulation.input import Input
 from sbaid.model.simulation.display import Display
-from sbaid.model.network.network import Network
+from sbaid.model.simulation.network_state import NetworkState
 
 
 class Algorithm(GObject.GInterface):
@@ -17,7 +17,7 @@ class Algorithm(GObject.GInterface):
         """todo"""
 
     def init(self, parameter_configuration_state: ParameterConfigurationState,
-             network_state: Network) -> None:
+             network_state: NetworkState) -> None:
         """todo"""
 
     def calculate_display(self, algorithm_input: list[Input]) -> Display:
