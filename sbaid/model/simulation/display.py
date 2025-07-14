90bc246c43a2cf76cdbb2fac33a06183bd035bae
Louis <uqyrq@student.kit.edu>
2025-07-14 22:22:46 +0200

    add display record-like functionality for a displays per cross-section per lane and b displays per cross-section.
    Return None if key is not found in the getter. Change the value if it is already set by the setter.

--- sbaid/model/simulation/display.py
+++ sbaid/model/simulation/display.py
@@ -6,18 +6,34 @@ from sbaid.common.b_display import BDisplay
 
 
 class Display(GObject.GObject):
-    """TODO"""
+    """
+    This class provides record-like funcionality of a display that contains a displays
+    per cross section per lane and b displays per cross section
+    """
+    _a_display: dict[str, dict[int, ADisplay]] = {}
+    _b_display: dict[str, BDisplay] = {}
+
+    def __init__(self):
+        super().__init__()
+        self._a_display = {}
+        self._b_display = {}
 
     def get_a_display(self, cross_section_id: str, lane_number: int) -> ADisplay:
-        """TODO"""
-        return ADisplay.OFF
+        """Get the A Display for the given cross section id and lane number. None if not found."""
+        return self._a_display.get(cross_section_id, {}).get(lane_number)
 
     def get_b_display(self, cross_section_id: str) -> BDisplay:
-        """TODO"""
-        return BDisplay.OFF
+        """Get the B Display for the given cross section id. None if not found."""
+        return self._b_display.get(cross_section_id)
 
     def set_a_display(self, cross_section_id: str, lane_number: int, display: ADisplay) -> None:
-        """TODO"""
+        """Set the A Display for the given cross section id and lane number."""
+        if cross_section_id not in self._a_display:
+            self._a_display[cross_section_id] = {}
+        self._a_display[cross_section_id][lane_number] = display
 
     def set_b_display(self, cross_section_id: str, display: BDisplay) -> None:
-        """TODO"""
+        """Set the B Display for the given cross section id."""
+        if cross_section_id not in self._a_display:
+            self._b_display[cross_section_id] = display
+        self._b_display[cross_section_id] = display
