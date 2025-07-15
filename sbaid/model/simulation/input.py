d770976f024bca6f925f84f00832e616dc739c6b
Louis <uqyrq@student.kit.edu>
2025-07-15 18:13:54 +0200

    add input functionality and documentation

--- sbaid/model/simulation/input.py
+++ sbaid/model/simulation/input.py
@@ -1,4 +1,4 @@
-"""TODO"""
+"""This module """
 from typing import Optional, List
 from gi.repository import GObject
 from sbaid.model.simulation.vehicle_info import VehicleInfo
@@ -6,18 +6,39 @@ from sbaid.common.vehicle_type import VehicleType
 
 
 class Input(GObject.GObject):
-    """TODO"""
+    """
+    This class stores information about the state of the traffic within the simulation.
+    It is possible to access the information directly but also derive secondary attributes.
+    """
+    _all_vehicle_infos: dict[str, dict[int, list[VehicleInfo]]] = {}
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._all_vehicle_infos: dict[str, dict[int, list[VehicleInfo]]] = {}
 
     def get_average_speed(self, cross_section_id: str, lane_number: int) -> Optional[float]:
-        """TODO"""
+        """Return the average speed of all vehicles at the given cross-section and lane."""
+        if len(self._all_vehicle_infos.get(cross_section_id, {}).get(lane_number, [])) == 0:
+            return None
+        all_speeds_sum: float = 0.0
+        for vehicle_info in self._all_vehicle_infos.get(cross_section_id, {}).get(lane_number, []):
+            all_speeds_sum += vehicle_info.speed
+        result: float = (all_speeds_sum
+                         / float(len(self._all_vehicle_infos
+                                     .get(cross_section_id, {}).get(lane_number, []))))
+        return result
 
     def get_traffic_volume(self, cross_section_id: str, lane_number: int) -> Optional[float]:
-        """TODO"""
+        # TODO: should this really be a float?
+        """Return the amount of vehicles at the given cross-section and lane."""
+        return len(self._all_vehicle_infos.get(cross_section_id, {}).get(lane_number, {}))
 
     def get_all_vehicle_infos(self, cross_section_id: str, lane_number: int) -> List[VehicleInfo]:
-        """TODO"""
-        return []
+        """Return a list of all vehicle information at the given cross-section and lane."""
+        return self._all_vehicle_infos.get(cross_section_id, {}).get(lane_number, []).copy()
 
     def add_vehicle_info(self, cross_section_id: str, lane_number: int, vehicle_type: VehicleType,
                          speed: float) -> None:
-        """TODO"""
+        """Add a new vehicle information to the given cross-section and lane."""
+        (self._all_vehicle_infos.get(cross_section_id, {}).get(lane_number, [])
+         .append(VehicleInfo(vehicle_type, speed)))
