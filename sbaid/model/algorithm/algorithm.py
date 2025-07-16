829d61e996d837bc3061ec8b8b315346abe83077
Louis <uqyrq@student.kit.edu>
2025-07-16 18:03:42 +0200

    make Algorithm a GObject

--- sbaid/model/algorithm/algorithm.py
+++ sbaid/model/algorithm/algorithm.py
@@ -1,14 +1,15 @@
 # mypy: disable-error-code="empty-body"
 """todo"""
 
-from gi.repository import Gio
+from gi.repository import Gio, GObject
+
 from sbaid.model.simulation.parameter_configuration_state import ParameterConfigurationState
 from sbaid.model.simulation.input import Input
 from sbaid.model.simulation.display import Display
 from sbaid.model.simulation.network_state import NetworkState
 
 
-class Algorithm:
+class Algorithm(GObject.GObject):
     """todo"""
 
     def get_global_parameter_template(self) -> Gio.ListModel:
