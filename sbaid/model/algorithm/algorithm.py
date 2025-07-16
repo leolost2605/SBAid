1b4cfb004d68dcced77e64586668d8eab3b7a937
Louis <uqyrq@student.kit.edu>
2025-07-16 17:25:00 +0200

    make algorithm not a GInterface

--- sbaid/model/algorithm/algorithm.py
+++ sbaid/model/algorithm/algorithm.py
@@ -1,14 +1,14 @@
 # mypy: disable-error-code="empty-body"
 """todo"""
 
-from gi.repository import Gio, GObject
+from gi.repository import Gio
 from sbaid.model.simulation.parameter_configuration_state import ParameterConfigurationState
 from sbaid.model.simulation.input import Input
 from sbaid.model.simulation.display import Display
 from sbaid.model.simulation.network_state import NetworkState
 
 
-class Algorithm(GObject.GInterface):
+class Algorithm:
     """todo"""
 
     def get_global_parameter_template(self) -> Gio.ListModel:
