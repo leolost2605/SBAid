938dbac27c761215437d20d95fd5523378ed97f5
Leonhard Kargl <leo.kargl@proton.me>
2025-07-16 19:22:51 +0200

    Simulator: Make methods async

--- sbaid/model/simulator/simulator.py
+++ sbaid/model/simulator/simulator.py
@@ -24,41 +24,41 @@ class Simulator(GObject.GInterface):
     def cross_sections(self) -> Gio.ListModel:
         """TODO"""
 
-    def load_file(self, file: Gio.File) -> None:
+    async def load_file(self, file: Gio.File) -> None:
         """Load the simulation file."""
 
-    def create_cross_section(self, coordinate: Coordinate,
+    async def create_cross_section(self, coordinate: Coordinate,
                              cross_section_type: CrossSectionType) -> int:
         """
         Create a cross section object, add it to the cross section list
         and return its position within the list
         """
 
-    def remove_cross_section(self, cross_section_id: str) -> None:
+    async def remove_cross_section(self, cross_section_id: str) -> None:
         """Remove the cross section object."""
 
-    def move_cross_section(self, cross_section_id: str,
+    async def move_cross_section(self, cross_section_id: str,
                            new_position: Coordinate) -> None:
         """Move the cross section object."""
 
-    def init_simulation(self) -> tuple[int, int]:
+    async def init_simulation(self) -> tuple[int, int]:
         """
         Initialize the simulation object. Return the internal simulation start time
         and runtime.
         """
         # TODO GLib.TimeSpan is an alias for int in PyGObject, is this a problem?
 
-    def continue_simulation(self, span: int) -> None:
+    async def continue_simulation(self, span: int) -> None:
         """
         Simulate the given timespan. If the timespan surpasses the simulation length
         only simulate until the end of the simulation length.
         """
 
-    def measure(self) -> Input:
+    async def measure(self) -> Input:
         """Collect measurement data, return as an Input object."""
 
-    def set_display(self, display: Display) -> None:
+    async def set_display(self, display: Display) -> None:
         """Set the display."""
 
-    def stop_simulation(self) -> None:
+    async def stop_simulation(self) -> None:
         """Stop the simulation."""
