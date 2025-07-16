36e4d9703ac4714547e2dd66e9f4f28764317214
mariag2804 <151514006+mariag2804@users.noreply.github.com>
2025-07-16 16:30:40 +0200

    Network Skeleton

---
+++ sbaid/model/network/route.py
@@ -0,0 +1,19 @@
+"""TODO"""
+from typing import List
+
+from gi.repository import GObject
+from gi.repository import Gio
+
+from sbaid.common.coordinate import Coordinate
+
+
+class Route:
+    """TODO"""
+    points = GObject.Property(type=Gio.ListModel,
+                              flags=GObject.ParamFlags.READABLE |
+                              GObject.ParamFlags.WRITABLE |
+                              GObject.ParamFlags.CONSTRUCT_ONLY)
+
+    def __init__(self, coordinates: List[Coordinate]) -> None:
+        # TODO: turn list into listmodel
+        """TODO"""
