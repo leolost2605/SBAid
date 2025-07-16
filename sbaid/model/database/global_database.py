a4b1860f75a20a7b0f9c4d4657e2c5520b020067
Louis <uqyrq@student.kit.edu>
2025-07-16 18:43:20 +0200

    change GInterfaces to ABC, remove unnecessary returns

--- sbaid/model/database/global_database.py
+++ sbaid/model/database/global_database.py
@@ -1,5 +1,6 @@
 """TODO"""
-from gi.repository.GObject import GInterface
+from abc import ABC, abstractmethod
+
 from gi.repository.GLib import DateTime
 from gi.repository.Gio import File
 
@@ -9,73 +10,83 @@ from sbaid.common.simulator_type import SimulatorType
 from sbaid.common.vehicle_type import VehicleType
 
 
-class GlobalDatabase(GInterface):
+class GlobalDatabase(ABC):
     """TODO"""
 
+    @abstractmethod
     def open(self, file: File) -> None:
         """TODO"""
 
+    @abstractmethod
     def add_project(self, project_id: str, simulator_type: SimulatorType,
                     simulator_file_path: str, project_file_path: str) -> None:
         """TODO"""
+
+    @abstractmethod
     def get_all_projects(self) -> list[tuple[str, SimulatorType, str, str,]]:
         """TODO"""
-        return []
 
+    @abstractmethod
     def remove_project(self, project_id: str) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_all_results(self) -> list[tuple[str, DateTime]]:
         """TODO"""
-        return []
 
+    @abstractmethod
     def save_result(self, result_id: str, project_name: str, creation_date_time: DateTime) -> None:
         """TODO"""
 
+    @abstractmethod
     def delete_result(self, result_id: str) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_result_name(self, result_id: str) -> str:
         """TODO"""
-        return ""
 
+    @abstractmethod
     def get_all_tags(self) -> list[str]:
         """TODO"""
-        return []
 
+    @abstractmethod
     def get_result_tags(self, result_id: str) -> list[str]:
         """TODO"""
-        return []
 
+    @abstractmethod
     def get_all_snapshots(self, result_id: str) -> list[tuple[str, DateTime]]:
         """TODO"""
-        return []
 
+    @abstractmethod
     def save_snapshot(self, result_id: str, snapshot_id: str, time: DateTime) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_all_cross_section_snapshots(self, snapshot_id: str) -> list[tuple[str, str, BDisplay]]:
         """TODO"""
-        return []
 
+    @abstractmethod
     def save_cross_section_snapshot(self, snapshot_id: str, cross_section_snapshot_ic: str,
                                     time: DateTime) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_all_lane_snapshots(self, cross_section_snapshot_id: str)\
             -> list[tuple[str, int, float, int, ADisplay]]:
         """TODO"""
-        return []
 
+    @abstractmethod
     def save_lane_snapshot(self, cross_section_snapshot_id: str, lane_snapshot_id: str,
                            lane: int, average_speed: float, traffic_volume: int,
                            a_display: ADisplay) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_all_vehicle_snapshots(self, lane_snapshot_id: str) -> list[tuple[VehicleType, float]]:
         """TODO"""
-        return []
 
+    @abstractmethod
     def save_vehicle_snapshot(self, lane_snapshot_id: str,
                               vehicle_type: VehicleType, speed: float) -> None:
         """TODO"""
--- sbaid/model/database/project_database.py
+++ sbaid/model/database/project_database.py
@@ -1,93 +1,106 @@
 """TODO"""
-from typing import Optional
+from abc import ABC, abstractmethod
 
-from gi.repository.GObject import GInterface
 from gi.repository.GLib import Variant
 from gi.repository.GLib import DateTime
 from gi.repository.Gio import File
 
 
-class ProjectDatabase(GInterface):
+class ProjectDatabase(ABC):
     """TODO"""
 
+    @abstractmethod
     def open(self, file: File) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_created_at(self) -> DateTime:
         """TODO"""
         return DateTime()
 
+    @abstractmethod
     def get_last_modified(self) -> DateTime:
         """TODO"""
         return DateTime()
 
+    @abstractmethod
     def set_last_modified(self, new_last_modified: DateTime) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_project_name(self) -> str:
         """TODO"""
-        return ""
 
+    @abstractmethod
     def set_project_name(self, name: str) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_cross_section_name(self, cross_section_id: str) -> str:
         """TODO"""
         return ""
 
+    @abstractmethod
     def set_cross_section_name(self, name: str) -> None:
         """TODO"""
-        return ""
 
+    @abstractmethod
     def get_algorithm_configuration_name(self, algorithm_configuration_id: str) -> str:
         """TODO"""
-        return ""
 
+    @abstractmethod
     def set_algorithm_configuration_name(self, name: str) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_algorithm_configuration(self, algorithm_configuration_id: str) \
             -> tuple[str, int, int, str]:
         """TODO"""
-        return "", 0, 0, ""
 
+    @abstractmethod
     def get_all_algorithm_configuration_ids(self) -> list[str]:
         """TODO"""
         return []
 
+    @abstractmethod
     def get_selected_algorithm_configuration_id(self) -> str:
         """TODO"""
-        return ""
 
+    @abstractmethod
     def set_selected_algorithm_configuration_id(self, configuration_id: str) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_display_interval(self, algorithm_configuration_id: str) -> int:
         """TODO"""
-        return 0
 
+    @abstractmethod
     def set_display_interval(self, algorithm_configuration_id: str, interval: int) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_evaluation_interval(self, algorithm_configuration_id: str) -> int:
         """TODO"""
         return 0
 
+    @abstractmethod
     def set_evaluation_interval(self, algorithm_configuration_id: str, interval: int) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_script_path(self, algorithm_configuration_id: str) -> str:
         """TODO"""
-        return ""
 
+    @abstractmethod
     def set_script_path(self, algorithm_configuration_id: str, path: str) -> None:
         """TODO"""
 
+    @abstractmethod
     def get_parameter_value(self, algorithm_configuration_id: str, parameter_name: str,
-                            cross_section_id: Optional[str]) -> str:
+                            cross_section_id: str | None) -> str:
         """TODO"""
-        return ""
 
+    @abstractmethod
     def set_parameter_value(self, algorithm_configuration_id: str, parameter_name: str,
-                            cross_section_id: Optional[str], parameter_value: Variant) -> None:
+                            cross_section_id: str | None, parameter_value: Variant) -> None:
         """TODO"""
