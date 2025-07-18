88ca1b7e7d202294f392a46c971f2c4ae9a17ecb
Louis <uqyrq@student.kit.edu>
2025-07-18 03:21:48 +0200

    add getter by key

--- sbaid/common/b_display.py
+++ sbaid/common/b_display.py
@@ -11,3 +11,14 @@ class BDisplay(GObject.GEnum):
     CAUTION_FOG = 4
     SNOW = 5
     SLIPPERY_ROAD = 6
+
+def get_by_number(key):
+    match key:
+        case '0': return BDisplay.OFF
+        case '1': return BDisplay.TRAFFIC_JAM
+        case '2': return BDisplay.CONSTRUCTION
+        case '3': return BDisplay.CAUTION_ACCIDENT
+        case '4': return BDisplay.CAUTION_FOG
+        case '5': return BDisplay.SNOW
+        case '6': return BDisplay.SLIPPERY_ROAD
+    return None
