b84325ad4f0b918d740d1aace2b73f236ca9df98
Louis <uqyrq@student.kit.edu>
2025-07-18 03:22:02 +0200

    fixes

--- sbaid/model/database/global_sqlite.py
+++ sbaid/model/database/global_sqlite.py
@@ -2,9 +2,10 @@
 import datetime
 import sqlite3
 
-from gi.repository import GLib, Gio
+from gi.repository import GLib, Gio, GObject
 from gi.repository.GLib import TimeZone
 
+from sbaid.common import b_display, a_display
 from sbaid.common.a_display import ADisplay
 from sbaid.common.b_display import BDisplay
 from sbaid.common.simulator_type import SimulatorType
@@ -24,14 +25,14 @@ class GlobalSQLite(GlobalDatabase):
 
         self._connection = sqlite3.connect(path, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
         
-        self._connection.cursor().executescript("""DROP TABLE project;
-DROP TABLE result;
-DROP TABLE result_tag;
-DROP TABLE tag;
-DROP TABLE snapshot;
-DROP TABLE cross_section_snapshot;
-DROP TABLE lane_snapshot;
-DROP TABLE vehicle_snapshot;
+        self._connection.cursor().executescript("""DROP TABLE IF EXISTS project;
+DROP TABLE IF EXISTS result;
+DROP TABLE IF EXISTS result_tag;
+DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS snapshot;
+DROP TABLE IF EXISTS cross_section_snapshot;
+DROP TABLE IF EXISTS lane_snapshot;
+DROP TABLE IF EXISTS vehicle_snapshot;
 
 CREATE TABLE project (
     id TEXT PRIMARY KEY,
@@ -89,22 +90,20 @@ CREATE TABLE vehicle_snapshot (
                     simulator_file_path: str, project_file_path: str) -> None:
         self._connection.cursor().execute(f"""
         INSERT INTO project (id, simulator_type_id, simulator_type_name, simulator_file_path, project_file_path)
-        VALUES ('{project_id}', '{simulator_type.id}', '{simulator_type.name}', '{simulator_file_path}', '{project_file_path}');
-        """)
+        VALUES (?, ?, ?, ?, ?);
+        """, (project_id, simulator_type.id, simulator_type.name, simulator_file_path, project_file_path))
 
     async def get_all_projects(self) -> list[tuple[str, SimulatorType, str, str,]]:
         """TODO"""
         def func(db_input: tuple[str, str, str, str, str]) -> tuple[str, SimulatorType, str, str,]:
             return db_input[0], SimulatorType(db_input[1], db_input[2]), db_input[3], db_input[4]
-        return list(map(func, self._connection.cursor().execute(f"""
+        return list(map(func, self._connection.cursor().execute("""
         SELECT * FROM project;
         """).fetchall()))
 
     async def remove_project(self, project_id: str) -> None:
         """TODO"""
-        self._connection.cursor().execute(f"""
-        DELETE FROM project WHERE id = '{project_id}';
-        """)
+        self._connection.cursor().execute("DELETE FROM project WHERE id = ?;", (project_id,))
 
     async def get_all_results(self) -> list[tuple[str, GLib.DateTime]]:
         """TODO"""
@@ -118,27 +117,28 @@ CREATE TABLE vehicle_snapshot (
     async def save_result(self, result_id: str, result_name: str, project_name: str,
                           creation_date_time: GLib.DateTime) -> None:
         """TODO"""
-        self._connection.cursor().execute(f"""
+        self._connection.cursor().execute("""
         INSERT INTO result (id, name, project_name, date)
-        VALUES ('{result_id}', '{result_name}', '{project_name}', '{glib_to_datetime(creation_date_time)}');
-        """)
+        VALUES (?, ?, ?, ?);
+        """, (result_id, result_name, project_name, glib_to_datetime(creation_date_time)))
 
 
     async def delete_result(self, result_id: str) -> None:
         """TODO"""
-        self._connection.cursor().execute(f"""
-        DELETE FROM result WHERE id = '{result_id}';
-        """)
+        self._connection.cursor().execute("""
+        DELETE FROM result WHERE id = ?;
+        """, (result_id,)).close()
 
     async def get_result_name(self, result_id: str) -> str:
         """TODO"""
-        all_results = self._connection.cursor().execute(f"""
-        SELECT name FROM result WHERE id = '{result_id}';
-        """).fetchall()[0]
+        all_results = self._connection.cursor().execute("""
+        SELECT name FROM result WHERE id = ?;
+        """, (result_id,)).fetchall()[0]
         if len(all_results) == 0:
             return all_results[0]
         else:
-            raise RuntimeError  # TODO
+            return all_results[0]
+            # raise sqlite3.OperationalError  # TODO
 
     async def get_all_tags(self) -> list[tuple[str, str]]:
         """TODO"""
@@ -148,9 +148,9 @@ CREATE TABLE vehicle_snapshot (
 
     async def get_result_tags(self, result_id: str) -> list[str]:
         """TODO"""
-        return self._connection.cursor().execute(f"""
-        SELECT id FROM result_tag WHERE result_id = '{result_id}';
-        """).fetchall()
+        return self._connection.cursor().execute("""
+        SELECT id FROM result_tag WHERE result_id = ?;
+        """, (result_id,)).fetchall()
 
     async def get_all_snapshots(self, result_id: str) -> list[tuple[str, GLib.DateTime]]:
         """TODO"""
@@ -163,44 +163,45 @@ CREATE TABLE vehicle_snapshot (
 
     async def save_snapshot(self, snapshot_id: str, time: GLib.DateTime, result_id: str) -> None:
         """TODO"""
-        self._connection.cursor().execute(f"""
+        self._connection.cursor().execute("""
         INSERT INTO snapshot (id, date, result_id)
-        VALUES ('{result_id}', '{glib_to_datetime(time)}', '{snapshot_id}'');
-        """)
+        VALUES (?, ?, ?);
+        """, (snapshot_id, glib_to_datetime(time), result_id))
 
     async def get_all_cross_section_snapshots(self, snapshot_id: str) -> list[tuple[str, str, BDisplay, str]]:
         """TODO"""
         def func(values: tuple[str, str, str, str]) -> tuple[str, str, BDisplay, str]:
-            return values[0], values[1], BDisplay(values[2]), values[3]
-        return list(map(func, (self._connection.cursor().execute(f"""
-        SELECT * FROM cross_section_snapshot WHERE snapshot_id = '{snapshot_id}';
-        """).fetchall())))
+            return values[0], values[1], b_display.get_by_number(values[2]), values[3]
+        return list(map(func, (self._connection.cursor().execute("""
+        SELECT * FROM cross_section_snapshot WHERE snapshot_id = ?;
+        """, (snapshot_id,)).fetchall())))
 
     async def save_cross_section_snapshot(self, cross_section_snapshot_id: str,
                                     cross_section_name: str, b_display: BDisplay,
                                           snapshot_id: str,) -> None:
         """TODO"""
-        self._connection.cursor().execute(f"""
+        self._connection.cursor().execute("""
         INSERT INTO cross_section_snapshot (id, cross_section_name, b_display, snapshot_id)
-        VALUES ('{cross_section_snapshot_id}', '{cross_section_name}', '{str(b_display)}', '{snapshot_id}');
-        """)
+        VALUES (?, ?, ?, ?);
+        """, (cross_section_snapshot_id, cross_section_name, str(b_display), snapshot_id))
 
     async def get_all_lane_snapshots(self, cross_section_snapshot_id: str)\
             -> list[tuple[str, int, ADisplay, str]]:
         """TODO"""
         def func(values: tuple[str, int, str, str]) -> tuple[str, int, ADisplay, str]:
-            return values[0], values[1], ADisplay(values[2]), values[3]
-        return list(map(func, self._connection.cursor().execute(f"""
-        SELECT * FROM lane_snapshot WHERE snapshot_id = '{cross_section_snapshot_id}';
-        """).fetchall()))
+            adisp = a_display.get_by_number(values[2])
+            return values[0], values[1], a_display.get_by_number(values[2]), values[3]
+        return list(map(func, self._connection.cursor().execute("""
+        SELECT * FROM lane_snapshot WHERE cross_section_snapshot_id = ?;
+        """, (cross_section_snapshot_id,)).fetchall()))
 
     async def save_lane_snapshot(self, lane_snapshot_id: str, lane: int, cross_section_snapshot_id: str,
                            a_display: ADisplay) -> None:
         """TODO"""
-        self._connection.cursor().execute(f"""
-        INSERT INTO lane_snapshot ('{lane_snapshot_id}', '{lane}', '{str(a_display)}',
-        '{cross_section_snapshot_id});
-        """)
+        self._connection.cursor().execute("""
+        INSERT INTO lane_snapshot (id, lane_number, a_display, cross_section_snapshot_id) 
+        VALUES (?, ?, ?, ?);
+        """, (lane_snapshot_id, lane, str(a_display), cross_section_snapshot_id))
 
     async def get_all_vehicle_snapshots(self, lane_snapshot_id: str)\
             -> list[tuple[VehicleType, float]]:
@@ -209,17 +210,16 @@ CREATE TABLE vehicle_snapshot (
         def func(values: tuple[str, float]) -> tuple[VehicleType, float]:
             return VehicleType(values[0]), values[1]
 
-        return list(map(func, self._connection.cursor().execute(f"""
-        SELECT * FROM vehicle_snapshot WHERE lane_snapshot_id = '{lane_snapshot_id}';
-        """).fetchall()))
+        return list(map(func, self._connection.cursor().execute("""
+        SELECT * FROM vehicle_snapshot WHERE lane_snapshot_id = ?;
+        """, (lane_snapshot_id,)).fetchall()))
 
     async def save_vehicle_snapshot(self, vehicle_snapshot_id: str, lane_snapshot_id: str,
                               vehicle_type: VehicleType, speed: float) -> None:
         """TODO"""
-        self._connection.cursor().execute(f"""
-        INSERT INTO vehicle_snapshot ('{vehicle_snapshot_id}', '{str(vehicle_type)}', '{speed}',
-        '{lane_snapshot_id});
-        """)
+        self._connection.cursor().execute("""
+        INSERT INTO vehicle_snapshot (?, ?, ?, ?);
+        """, (vehicle_snapshot_id, str(vehicle_type), speed, lane_snapshot_id))
 
 
 def glib_to_datetime(glib_date: GLib.DateTime) -> datetime.datetime:
--- tests/model/database/test_global_sqlite.py
+++ tests/model/database/test_global_sqlite.py
@@ -1,10 +1,10 @@
-import asyncio
 import unittest
-from time import tzset
 
 from gi.repository import Gio
 from gi.repository.GLib import DateTime, TimeZone
 
+from sbaid.common.a_display import ADisplay
+from sbaid.common.b_display import BDisplay
 from sbaid.common.simulator_type import SimulatorType
 from sbaid.model.database.global_sqlite import GlobalSQLite
 
@@ -38,11 +38,77 @@ class GlobalSQLiteTest(unittest.TestCase):
                              "my_project_name", DateTime.new_now(TimeZone.new("Europe/Berlin")))
         all_results = await db.get_all_results()
         self.assertEqual(len(all_results), 1)
+        self.assertEqual(await db.get_result_name("my_result_id"), "my_result_name")
+        await db.delete_result("my_result_id")
+        all_results = await db.get_all_results()
+        self.assertEqual(len(all_results), 0)
+
+
+    async def test_snapshot(self):
+        db = GlobalSQLite()
+        file = Gio.File.new_for_path("/Users/fuchs/PycharmProjects/SBAid/sbaid/model/database/test.db")
+        await db.open(file)
+        await db.add_project("my_project_id", SimulatorType("0", "Vissim"),
+                             "my_simulator_file_path",
+                             "my_project_file_path")
+        await db.save_result("my_result_id", "my_result_name",
+                             "my_project_name", DateTime.new_now(TimeZone.new("Europe/Berlin")))
+
+        self.assertEqual(len(await db.get_all_snapshots("my_result_id")), 0)
+
+        await db.save_snapshot("my_snapshot_id", DateTime.new_now(TimeZone.new("Europe/Berlin")),
+                               "my_result_id")
+
+        self.assertEqual(len(await db.get_all_snapshots("my_snapshot_id")), 1)
+
+    async def test_cross_section_snapshot(self):
+        db = GlobalSQLite()
+        file = Gio.File.new_for_path("/Users/fuchs/PycharmProjects/SBAid/sbaid/model/database/test.db")
+        await db.open(file)
+        await db.add_project("my_project_id", SimulatorType("0", "Vissim"),
+                             "my_simulator_file_path",
+                             "my_project_file_path")
+        await db.save_result("my_result_id", "my_result_name",
+                             "my_project_name", DateTime.new_now(TimeZone.new("Europe/Berlin")))
+        await db.save_cross_section_snapshot("my_cross_section_snapshot_id",
+                                             "my_cross_section_name", BDisplay.OFF,
+                                             "my_snapshot_id")
+        self.assertEqual(len(await db.get_all_cross_section_snapshots("my_snapshot_id")), 1)
+        await db.save_cross_section_snapshot("my_cross_section_snapshot_id_2",
+                                             "my_cross_section_name", BDisplay.OFF,
+                                             "my_snapshot_id")
+        self.assertEqual(len(await db.get_all_cross_section_snapshots("my_snapshot_id")), 2)
+
+        # self.assertRaises(sqlite3.OperationalError, db.save_cross_section_snapshot, "my_cross_section_snapshot_id",
+        #                                      "my_cross_section_name", BDisplay.OFF,
+        #                                      "my_snapshot_id") # TODO should not work but does
+
+    async def test_lane_snapshot(self):
+        db = GlobalSQLite()
+        file = Gio.File.new_for_path("/Users/fuchs/PycharmProjects/SBAid/sbaid/model/database/test.db")
+        await db.open(file)
+        await db.add_project("my_project_id", SimulatorType("0", "Vissim"),
+                             "my_simulator_file_path",
+                             "my_project_file_path")
+        await db.save_result("my_result_id", "my_result_name",
+                             "my_project_name", DateTime.new_now(TimeZone.new("Europe/Berlin")))
+        await db.save_cross_section_snapshot("my_cross_section_snapshot_id",
+                                             "my_cross_section_name", BDisplay.OFF,
+                                             "my_snapshot_id")
+        await db.save_lane_snapshot("my_lane_snapshot_id", 1, "my_cross_section_snapshot_id", ADisplay.OFF)
+
+        self.assertEqual(len(await db.get_all_lane_snapshots("my_cross_section_snapshot_id")), 1)
+        cs_snapshot = await db.get_all_lane_snapshots("my_cross_section_snapshot_id")
+        self.assertEqual(cs_snapshot[0][2], ADisplay.OFF)
 
 
 
 
 
+# asyncio.run(GlobalSQLiteTest().test_remove())
+# asyncio.run(GlobalSQLiteTest().test_result())
+# asyncio.run(GlobalSQLiteTest().test_snapshot())
+# asyncio.run(GlobalSQLiteTest().test_cross_section_snapshot())
+# asyncio.run(GlobalSQLiteTest().test_lane_snapshot())
 
 
-asyncio.run(GlobalSQLiteTest().test_remove())
