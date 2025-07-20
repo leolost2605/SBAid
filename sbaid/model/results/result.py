b9e1c1b9ab90f7078ae52790a4e9eecf435cbf57
leonormsimoes <142237594+leonormsimoes@users.noreply.github.com>
2025-07-20 18:33:23 +0200

    noch ein zwischencommit

--- sbaid/model/results/result.py
+++ sbaid/model/results/result.py
@@ -43,6 +43,11 @@ class Result(GObject.GObject):
         flags=GObject.ParamFlags.READABLE |
         GObject.ParamFlags.WRITABLE |
         GObject.ParamFlags.CONSTRUCT_ONLY)
+    snapshots = GObject.Property(
+        type=Gio.ListModel,
+        flags=GObject.ParamFlags.READABLE |
+              GObject.ParamFlags.WRITABLE |
+              GObject.ParamFlags.CONSTRUCT_ONLY)
 
     def __init__(self, result_id: str, project_name: str,
                  creation_date_time: GLib.DateTime) -> None:
