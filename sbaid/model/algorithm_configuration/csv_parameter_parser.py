3ee0e560f99911a9ccf53c4c8398948339c900fe
deniz karabati <daphne41.17@gmail.com>
2025-07-21 14:11:56 +0200

    tried doing some logic for the parser, mostly coppied of leocools parser

--- sbaid/model/algorithm_configuration/csv_parameter_parser.py
+++ sbaid/model/algorithm_configuration/csv_parameter_parser.py
@@ -1,6 +1,6 @@
 """This module defines the CSVParameterParser class."""
+import csv
 from typing import Tuple
-
 from gi.repository import Gio
 from sbaid.model.algorithm_configuration.parameter_parser import (
     ParameterParser, ParameterParserForeachFunc)
@@ -11,9 +11,29 @@ class CSVParameterParser(ParameterParser):
 
     def can_handle(self, file: Gio.File) -> bool:
         """todo"""
-        return None
+        return Gio.content_type_guess(file.get_path())[0] == ".csv"
+
 
     def for_each_parameter(self, file: Gio.File,
                            callback: ParameterParserForeachFunc) -> Tuple[int, int]:
         """TODO"""
-        return 0, 0
+        with open(file.get_path(), newline='') as csvfile:
+            valid_parameters = 0
+            invalid_parameters = 0
+
+            csv_reader = csv.reader(csvfile)
+            try:
+                has_header = self.__has_valid_header(csv_reader)
+            except StopIteration:  # raised if the file is empty and there is no line to read from
+                raise InvalidFileFormattingException()
+
+        return valid_parameters, invalid_parameters
+
+    def __has_valid_header(self, csv_reader) -> bool:
+        """Control if first row = header = all parameter names are valid and unique"""
+
+        return None
+
+    def __cross_sections_valid(self, csv_reader) -> bool:
+        """Control if first column are all valid cross-section names"""
+        return None
