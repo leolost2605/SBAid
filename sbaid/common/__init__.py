90cd6a082620807bfed15e42ef10e48dfae7419f
Leonhard Kargl <leo.kargl@proton.me>
2025-07-22 16:24:17 +0200

    Make dir with parents async

--- sbaid/common/__init__.py
+++ sbaid/common/__init__.py
@@ -5,10 +5,22 @@ to all other sbaid modules.
 
 from typing import Any, Generator
 
-from gi.repository import Gio
+from gi.repository import Gio, GLib
 
 
 def list_model_iterator(model: Gio.ListModel) -> Generator[Any | None, Any, None]:
     """Iterates over a Gio.ListModel"""
     for i in range(model.get_n_items() - 1):
         yield model.get_item(i)
+
+
+async def make_directory_with_parents_async(dir: Gio.File | None) -> None:
+    if dir is None:
+        raise FileNotFoundError("Cannot create dir with parents. No root directory found.")
+
+    if dir.query_exists():
+        return
+
+    await make_directory_with_parents_async(dir.get_parent())
+
+    await dir.make_directory_async(GLib.PRIORITY_DEFAULT)
