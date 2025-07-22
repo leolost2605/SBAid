f30e6ff458d4699e0c4917b1022b862d3274772d
Louis <uqyrq@student.kit.edu>
2025-07-23 00:53:08 +0200

    add doc

--- sbaid/model/database/project_database.py
+++ sbaid/model/database/project_database.py
@@ -1,157 +1,157 @@
-"""TODO"""
+"""This module contains the ProjectDatabase interface."""
 from abc import ABC, abstractmethod
 
 from gi.repository import GLib
 
 
 class ProjectDatabase(ABC):
-    """TODO"""
-
-    # @abstractmethod
-    # async def open(self, file: Gio.File) -> None:
-    #     """TODO"""
+    """This interface contains methods that encapsulate necessary database behaviour."""
 
     @abstractmethod
     async def get_created_at(self) -> GLib.DateTime:
-        """TODO"""
+        """Return the GLib.DateTime when the project was created."""
 
     @abstractmethod
     async def set_last_modified(self, new_last_modified: GLib.DateTime) -> None:
-        """TODO"""
+        """Update the GLib.DateTime when the project was last modified."""
 
     @abstractmethod
     async def get_project_name(self) -> str:
-        """TODO"""
+        """Return the name of the project."""
 
     @abstractmethod
     async def set_project_name(self, name: str) -> None:
-        """TODO"""
+        """Update the name of the project."""
 
     @abstractmethod
     async def get_cross_section_name(self, cross_section_id: str) -> str:
-        """TODO"""
+        """Return the name of the cross_section with the given id."""
 
     @abstractmethod
     async def set_cross_section_name(self, cross_section_id: str, name: str) -> None:
-        """TODO"""
+        """Update the name of the cross_section with the given id."""
 
     @abstractmethod
     async def get_algorithm_configuration_name(self, algorithm_configuration_id: str) -> str:
-        """TODO"""
+        """Return the name of the algorithm_configuration with the given id."""
 
     @abstractmethod
     async def set_algorithm_configuration_name(self, algorithm_configuration_id: str,
                                                name: str) -> None:
-        """TODO"""
+        """Update the name of the algorithm_configuration with the given id."""
 
     @abstractmethod
     async def get_last_modified(self) -> GLib.DateTime:
-        """TODO"""
+        """Return the GLib.DateTime when the project was last modified."""
 
     @abstractmethod
     async def get_algorithm_configuration(self, algorithm_configuration_id: str) \
             -> tuple[str, str, int, int, str, bool]:
-        """TODO"""
+        """Return the algorithm_configuration with the given id."""
 
     @abstractmethod
     async def get_all_algorithm_configuration_ids(self) -> list[str]:
-        """TODO"""
+        """Return all algorithm_configuration ids."""
 
     @abstractmethod
     async def get_selected_algorithm_configuration_id(self) -> str:
-        """TODO"""
+        """Return the currently selected algorithm_configuration id."""
 
     @abstractmethod
     async def set_selected_algorithm_configuration_id(self, configuration_id: str) -> None:
-        """TODO"""
+        """Update the currently selected algorithm_configuration id."""
 
     @abstractmethod
     async def get_display_interval(self, algorithm_configuration_id: str) -> int:
-        """TODO"""
+        """Return the display interval of the given algorithm_configuration id."""
 
     @abstractmethod
     async def set_display_interval(self, algorithm_configuration_id: str, interval: int) -> None:
-        """TODO"""
+        """Update the display interval of the given algorithm_configuration id."""
 
     @abstractmethod
     async def get_evaluation_interval(self, algorithm_configuration_id: str) -> int:
-        """TODO"""
+        """Return the evaluation interval of the given algorithm_configuration id."""
 
     @abstractmethod
     async def set_evaluation_interval(self, algorithm_configuration_id: str,
                                       interval: int) -> None:
-        """TODO"""
+        """Update the evaluation interval of the given algorithm_configuration id."""
 
     @abstractmethod
     async def get_script_path(self, algorithm_configuration_id: str) -> str:
-        """TODO"""
+        """Return the scrip path of the given algorithm_configuration id."""
 
     @abstractmethod
     async def set_script_path(self, algorithm_configuration_id: str, path: str) -> None:
-        """TODO"""
+        """Update the script path of the given algorithm_configuration id."""
 
     @abstractmethod
     async def get_all_parameters(self, algorithm_configuration_id: str) -> list[tuple[str, str]]:
-        """TODO"""
+        """Return all parameters of the given algorithm_configuration id."""
 
     @abstractmethod
     async def get_parameter_value(self, algorithm_configuration_id: str, parameter_name: str,
                                   cross_section_id: str | None) -> GLib.Variant | None:
-        """TODO"""
+        """Return the value of the parameter of the given algorithm configuration
+        and parameter and possibly cross section."""
 
     @abstractmethod
     async def set_parameter_value(self, algorithm_configuration_id: str, parameter_name: str,
                                   cross_section_id: str | None,
                                   parameter_value: GLib.Variant) -> None:
-        """TODO"""
+        """Update the value of the parameter of the given algorithm configuration,
+        parameter name and cross section."""
 
     @abstractmethod
     async def add_cross_section(self, cross_section_id: str, name: str) -> None:
-        """TODO"""
+        """Add a new cross section with an id and a name."""
 
     @abstractmethod
     async def remove_cross_section(self, cross_section_id: str) -> None:
-        """TODO"""
+        """Remove a cross section from the database."""
 
     @abstractmethod
     async def add_parameter(self, algorithm_configuration_id: str, name: str,
                             cross_section_id: str | None, value: GLib.Variant) -> None:
-        """TODO"""
+        """Add a new parameter from the given algorithm configuration and parameter."""
 
     @abstractmethod
     async def remove_parameter(self, algorithm_configuration_id: str, name: str,
                                cross_section_id: str | None) -> None:
-        """TODO"""
+        """Remove a parameter with the given algorithm configuration and parameter name and possibly cross section."""
+
     @abstractmethod
     async def add_algorithm_configuration(self, algorithm_configuration_id: str, name: str,
                                           evaluation_interval: int, display_interval: int,
                                           script_path: str, is_selected: bool = True) -> None:
-        """TODO"""
+        """Add a new algorithm configuration to the database."""
 
     @abstractmethod
     async def remove_algorithm_configuration(self, algorithm_configuration_id: str) -> None:
-        """TODO"""
+        """Remove a algorithm configuration from the database."""
 
     @abstractmethod
     async def add_tag(self, tag_id: str, name: str) -> None:
-        """TODO"""
+        """Add a new tag to the database."""
 
     @abstractmethod
     async def remove_tag(self, tag_id: str) -> None:
-        """TODO"""
+        """Remove a tag from the database."""
 
     @abstractmethod
     async def add_parameter_tag(self, parameter_tag_id: str, parameter_name: str,
                                 algorithm_configuration_id: str, cross_section_id: str | None,
                                 tag_id: str) -> None:
-        """TODO"""
+        """Add a new parameter tag entry which represents a tag
+        belonging to the given parameter."""
 
     @abstractmethod
     async def remove_parameter_tag(self, parameter_tag_id: str) -> None:
-        """TODO"""
+        """Remove a parameter tag entry."""
 
     @abstractmethod
     async def get_all_tag_ids_for_parameter(self, algorithm_configuration_id: str,
                                             parameter_name: str,
                                             cross_section_id: str | None) -> list[str]:
-        """TODO"""
+        """Return all tag ids belonging to the given parameter."""
