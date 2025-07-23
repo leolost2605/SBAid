533b10dd4d0143b4921be03fb79cbdca0dd61a47
deniz karabati <daphne41.17@gmail.com>
2025-07-23 16:49:16 +0200

    very messy but it works, qv diagram

--- sbaid/model/results/qv_generator.py
+++ sbaid/model/results/qv_generator.py
@@ -1,6 +1,6 @@
-"""todo"""
+"""This module defines the QVGenerator class."""
 from matplotlib.colors import LinearSegmentedColormap
-
+from pandas import DataFrame
 from sbaid.common.diagram_type import DiagramType
 from sbaid.common.image import Image
 from sbaid.common.image_format import ImageFormat
@@ -8,8 +8,7 @@ from sbaid.model.results.cross_section_diagram_generator import CrossSectionDiag
 from sbaid.model.results.result import Result
 import seaborn as sns
 import matplotlib.pyplot as plt
-# import GlobalDatabase
-
+import pandas as pd
 
 class QVGenerator(CrossSectionDiagramGenerator):
     """todo"""
@@ -21,32 +20,67 @@ class QVGenerator(CrossSectionDiagramGenerator):
     def get_diagram(self, result: Result, cross_section_id: str,
                     export_format: ImageFormat) -> None:
         # cross_section_snapshot = GlobalDatabase.get_cross_section_snapshot(cross_section_id)
-        data = self.__filter_result_data(result, cross_section_id)
-        self.generate_diagram(data)
+        self.__generate_diagram(self.__extract_data(result, cross_section_id))
 
-        pass
 
-    def __filter_result_data(self, result: Result, cross_section_id) -> tuple[list, list]:
-        average_speed = []
-        traffic_density = []
+    def __extract_data(self, result: Result, cross_section_id) -> DataFrame:
+        """todo """
+        average_speed =[]
+        traffic_volume = []
+        a_displays = []
 
         for snapshot in result.snapshots:
             for cs_snapshot in snapshot.cross_section_snapshots:
                 if cs_snapshot.id == cross_section_id:
-                    average_speed.append(cs_snapshot.average_speed)
-                    traffic_density.append(cs_snapshot.traffic_density)
+                    for lane_snapshot in cs_snapshot.lane_snapshots:
+                        average_speed.append(lane_snapshot.average_speed)
+                        traffic_volume.append(lane_snapshot.traffic_volume)
+                        a_displays.append(lane_snapshot.a_display)
 
-        return average_speed, traffic_density
+        data = {
+            "speed": average_speed,
+            "volume": traffic_volume,
+            "display": a_displays
+        }
 
-    def generate_diagram(self, data: tuple[list, list]) -> Image:
+        return pd.DataFrame(data)
+
+
+    def __generate_diagram(self, data: DataFrame) -> Image:
         """todo help m"""
+
+        colorscheme = LinearSegmentedColormap.from_list('rg',
+                                         ["#910000", "#c10000", "r", "#ffa500", "y", "g"], N=256)
+
+        sns.set_theme(style="whitegrid")
+        ax = plt.subplot()
+        ax.set_xlabel("Q_pkv [Pkv/min]")
+        ax.set_ylabel("V[km/h]")
+
+        graph = sns.scatterplot(data=data, x="volume", y="speed", hue="display", palette=colorscheme, ax=ax)
+
+        plt.show()
+
+    def generate_diagram(self, average_speed: list, traffic_volume: list, a_displays: list) -> Image:
+        """TODO DELETE ONLY FOR TESTUNG"""
+
+        data = {
+            "speed": average_speed,
+            "volume": traffic_volume,
+            "display": a_displays
+        }
+
+        new_data = pd.DataFrame(data)
 
         colorscheme = LinearSegmentedColormap.from_list('rg',
                                                         ["#910000", "#c10000", "r", "#ffa500", "y", "g"], N=256)
 
+        sns.set_theme(style="whitegrid")
+        ax = plt.subplot()
+        ax.set_xlabel("Q_pkv [Pkv/min]")
+        ax.set_ylabel("V[km/h]")
 
-        average_speeds = data[0]
-        traffic_density = data[1]
-        ax = plt.subplot(figsize=(6.5, 6.5))
-        sns.scatterplot(x=average_speeds, y=traffic_density, ax=ax)
+        graph = sns.scatterplot(data=new_data, x="volume", y="speed", hue="display", palette=colorscheme, ax=ax)
         plt.show()
+
+        return None
--- tests/model/results/test_qv_diagram.py
+++ tests/model/results/test_qv_diagram.py
@@ -1,16 +1,18 @@
 import unittest
 from random import random
 
+from sbaid.common.a_display import ADisplay
 from sbaid.model.results.qv_generator import QVGenerator
 
 class QVDiagramTest(unittest.TestCase):
     def test_qv_(self):
         qv_generator = QVGenerator()
-        average_speed = list[int]
-        density = list[int]
+        average_speed = [80, 100, 85, 40, 45]
+        density = [50, 20, 50, 40, 45]
+        a_displays = [ADisplay.SPEED_LIMIT_100, ADisplay.SPEED_LIMIT_120, ADisplay.SPEED_LIMIT_80,
+                      ADisplay.SPEED_LIMIT_60, ADisplay.SPEED_LIMIT_60]
 
-        data = tuple[average_speed, density]
-        qv_generator.generate_diagram(data)
+        qv_generator.generate_diagram(average_speed, density, a_displays)
 
 
 if __name__ == '__main__':
