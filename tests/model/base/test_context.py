c5fb485b9db7454995b836c90a5b22a65cb9837e
mariag2804 <mariagavrilita04@gmail.com>
2025-07-25 14:41:28 +0200

    zwischencommit

--- tests/model/base/test_context.py
+++ tests/model/base/test_context.py
@@ -1,8 +1,14 @@
 """This module contains unittests for the Context class."""
 
 import unittest
+import uuid
+
+from sbaid.common.simulator_type import SimulatorType
+
 
 class ContextTestCase(unittest.TestCase):
     """This class tests the display using pythons unittest."""
 
-    def test_empty_display(self):
+    def test_create_project(self):
+        name = str(uuid.uuid4())
+        sim_type = SimulatorType(str(uuid.uuid4()), str(uuid.uuid4()))
--- tests/model/base/test_project.py
+++ tests/model/base/test_project.py
@@ -1,8 +1,19 @@
 """This module contains unittests for the Project class."""
 
 import unittest
+import uuid
+
+from sbaid.common.simulator_type import SimulatorType
+from sbaid.model.simulation_observer import SimulationObserver
+
 
 class ProjectTestCase(unittest.TestCase):
     """This class tests the display using pythons unittest."""
 
-    def test_empty_display(self):
+    __observer = SimulationObserver()
+    __project_id = str(uuid.uuid4())
+    __sim_type = SimulatorType(str(uuid.uuid4()), str(uuid.uuid4()))
+    __sim_file_path =
+    __project_file_path =
+
+def test_start_simulation(self):
