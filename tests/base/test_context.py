46de54b36a3cbb17b6fa0bff1a617fd54b1cd07c
Louis <uqyrq@student.kit.edu>
2025-07-31 23:33:05 +0200

    add load test

--- tests/base/test_context.py
+++ tests/base/test_context.py
@@ -4,7 +4,6 @@ import unittest
 
 from gi.events import GLibEventLoopPolicy
 
-import common
 from sbaid.common.simulator_type import SimulatorType
 from sbaid.model.context import Context
 
@@ -17,7 +16,26 @@ class ContextTestCase(unittest.TestCase):
         loop.run_until_complete(task)
 
     async def test(self) -> None:
+        await self.load()
         await self.projects()
+
+
+
+
+    async def load(self):
+        context1 = Context()
+        await context1.load()
+        proj_id = context1.create_project(
+            "test_project", SimulatorType("dummy_json", "JSON Dummy Simulator"),
+            "simulation_file_path", "tests/base/test_project")
+        # result manager is loaded from the db directly
+        context2 = Context()
+        await context2.load()
+        self.assertIsNotNone(context2.projects.get_item(0))
+        # self.assertEqual(context2.projects.get_item(0).id, proj_id)
+
+
+
     async def projects(self):
         context = Context()
         await context.load()
@@ -30,6 +48,8 @@ class ContextTestCase(unittest.TestCase):
         self.assertEqual(context.projects.get_n_items(), 2)
         self.assertEqual(context.projects.get_item(0).name, context.projects.get_item(1).name)
         self.assertNotEqual(context.projects.get_item(0).id, context.projects.get_item(1).id)
+
+
 
 
 
