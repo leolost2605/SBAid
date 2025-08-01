d00789ae49397cb11653cb1d94b76daf98f2ff5f
Leonhard Kargl <leo.kargl@proton.me>
2025-08-01 16:41:02 +0200

    test notify signal

--- tests/view_model/network/test_cross_section.py
+++ tests/view_model/network/test_cross_section.py
@@ -5,6 +5,9 @@ from sbaid.view_model.network.cross_section import CrossSection
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.__notified_name = False
+
     def test_property_notify(self):
         model_cs = mock.Mock()
         model_cs.id = "My Id"
@@ -14,6 +17,8 @@ class MyTestCase(unittest.TestCase):
         model_cs.b_display_active = True
 
         cs = CrossSection(model_cs)
+        cs.connect("notify::id", self.__on_notified)
+        cs.connect("notify::name", self.__on_notified)
 
         self.assertEqual(cs.name, "My Name")
         self.assertEqual(cs.hard_shoulder_available, False)
@@ -25,6 +30,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(cs.id, "My Id")
 
         cs.name = "My New Name"
+        self.assertEqual(self.__notified_name, True)
         self.assertEqual(cs.name, "My New Name")
 
         cs.hard_shoulder_available = True # This shouldn't be set
@@ -38,6 +44,10 @@ class MyTestCase(unittest.TestCase):
         cs.hard_shoulder_usable = True
 
         self.assertEqual(cs.hard_shoulder_usable, False)
+
+    def __on_notified(self, cs, pspec):
+        if pspec.name == "name":
+            self.__notified_name = True
 
 
 if __name__ == '__main__':
