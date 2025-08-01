980a2136b4909648bfe20ef7424ce8302f444a87
Louis <uqyrq@student.kit.edu>
2025-08-02 00:22:15 +0200

    move base tests to model directory

--- tests/model/base/test_context.py
+++ tests/model/base/test_context.py
@@ -1,27 +1,71 @@
-"""This module contains unittests for the Context class."""
+"""TODO"""
 import asyncio
 import unittest
 
 from gi.events import GLibEventLoopPolicy
+from gi.repository import Gio
 
-import sbaid.common
 from sbaid.common.simulator_type import SimulatorType
 from sbaid.model.context import Context
 
 
 class ContextTestCase(unittest.TestCase):
-    """This class tests the display using pythons unittest."""
 
-    def test_create_project(self):
-        context = Context()
+    def on_delete_async(feld, file, result, user_data):
+        file.delete_finish(result)
+
+
+    def test(self):
+        self.assertTrue(True)
         asyncio.set_event_loop_policy(GLibEventLoopPolicy())
-
         loop = asyncio.get_event_loop()
-
-        task = loop.create_task(context.load())
+        task = loop.create_task(ContextTestCase().start())
         loop.run_until_complete(task)
 
-        pr_id = context.create_project("my_other_name", SimulatorType("dummy simulator", "Dummy Simulator"),
-                               "my_simulation_file_path", "my_project_file_path")
+    async def start(self) -> None:
+        await self.load()
+        await self.projects()
 
-        self.assertEqual(2, context.projects.get_n_items())
+
+    async def load(self):
+        file = Gio.File.new_for_path("global_db")
+
+        context1 = Context()
+        await context1.load()
+
+        await context1.load()
+        proj_id = await context1.create_project(SimulatorType("dummy_json",
+                                                              "JSON Dummy Simulator"),
+                                                "simulation_file_path",
+                                                "tests/base/test_project")
+        # result manager is loaded from the db directly
+        context2 = Context()
+        await context2.load()
+        self.assertIsNotNone(context2.projects.get_item(0))
+        self.assertEqual(context2.projects.get_item(0).id, proj_id)
+
+        file.delete_async(0, None, self.on_delete_async, None)
+
+
+
+    async def projects(self):
+        file = Gio.File.new_for_path("global_db")
+
+
+        context = Context()
+        await context.load()
+        proj_id_1 = await context.create_project(SimulatorType("dummy_json", "JSON Dummy Simulator"),
+                               "simulation_file_path", "tests/base/test_project")
+        self.assertEqual(context.projects.get_item(0).id, proj_id_1)
+        proj_id_2 = await context.create_project(SimulatorType("dummy_json", "JSON Dummy Simulator"),
+                               "simulation_file_path", "tests/base/test_project")
+        self.assertEqual(context.projects.get_n_items(), 2)
+        self.assertNotEqual(context.projects.get_item(0).id, context.projects.get_item(1).id)
+
+
+        await context.delete_project(proj_id_1)
+        self.assertEqual(context.projects.get_item(0).id, proj_id_2)
+
+        file.delete_async(0, None, self.on_delete_async, None)
+
+        self.assertEqual(context.projects.get_item(0).id, proj_id_2)
--- tests/model/base/test_project.py
+++ tests/model/base/test_project.py
@@ -1,19 +1,85 @@
-"""This module contains unittests for the Project class."""
-
+"""TODO"""
+import asyncio
 import unittest
-import uuid
+from unittest import mock
 
+from gi.events import GLibEventLoopPolicy
+from gi.repository import GLib, Gio, GObject
+
+from model.algorithm_configuration.algorithm_configuration import AlgorithmConfiguration
+from model.algorithm_configuration.algorithm_configuration_manager import AlgorithmConfigurationManager
+from model.algorithm_configuration.parameter_configuration import ParameterConfiguration
+from model.network.network import Network
+from model.project import Project, AlgorithmConfigurationException
+from model.simulator.dummy.dummy_simulator import DummySimulator
 from sbaid.common.simulator_type import SimulatorType
-from sbaid.model.simulation_observer import SimulationObserver
+from sbaid.model.context import Context
 
 
 class ProjectTestCase(unittest.TestCase):
-    """This class tests the display using pythons unittest."""
 
-    __observer = SimulationObserver()
-    __project_id = str(uuid.uuid4())
-    __sim_type = SimulatorType(str(uuid.uuid4()), str(uuid.uuid4()))
-    __sim_file_path =
-    __project_file_path =
+    def on_delete_async(feld, file, result, user_data):
+        file.delete_finish(result)
 
-def test_start_simulation(self):
+
+    def test(self):
+        self.assertTrue(True)
+        asyncio.set_event_loop_policy(GLibEventLoopPolicy())
+        loop = asyncio.get_event_loop()
+        task = loop.create_task(ProjectTestCase().start())
+        loop.run_until_complete(task)
+
+    async def start(self) -> None:
+        # await self.start_simulation() TODO doesn't work without algorithm configuration
+        await self.load_from_db()
+        # load is not tested as it only delegates to network and
+        # algorithm configuration manager loading
+
+
+    async def load_from_db(self) -> None:
+        sim_type = SimulatorType("dummy_json", "Dummy Simulator")
+        context = Context()
+        await context.load()
+        await context.create_project(sim_type, "sim_file_path", "proj_file_path")
+        project = context.projects.get_item(0)
+        project.name = "my_cool_name"
+        created_at = project.created_at
+
+        self.assertEqual(project.name, "my_cool_name")
+
+        context2 = Context()
+        await context2.load()
+        project2 = context.projects.get_item(0)
+        self.assertEqual("my_cool_name", project2.name)
+        self.assertEqual(created_at, project2.created_at)
+
+
+        await Gio.File.new_for_path("proj_file_path").delete_async(0)
+        await Gio.File.new_for_path("global_db").delete_async(0)
+
+
+    async def start_simulation(self):
+        context = mock.Mock()
+        context.load()
+        observer = mock.Mock()
+        file = Gio.File.new_for_path("global_db")
+
+        sim = DummySimulator()
+        db = mock.Mock()
+        network = Network(sim, db)
+        algo_config_manager = AlgorithmConfigurationManager(network)
+        algo_config = AlgorithmConfiguration("config_id", network)
+        # algo_config.parameter_configuration = ParameterConfiguration(network)
+        # algo_config = algo_config_manager.create_algorithm_configuration()
+        project = Project(GLib.uuid_string_random(),
+                          SimulatorType("dummy_json", "JSON Dummy Simulator"),
+                          "simulation_file_path", "tests/base/test_project")
+        await project.load()
+        # should fail as no algo config is selected
+        self.assertRaises(AlgorithmConfigurationException, project.start_simulation, observer)
+        # algo_config.parameter_configuration = mock.Mock()
+        project.algorithm_configuration_manager.algorithm_configurations.append(algo_config)
+        project.algorithm_configuration_manager\
+            .selected_algorithm_configuration_id = "my_config_id"
+        simulation_manager = project.start_simulation(observer)
+        await simulation_manager.start()
