ed454a14806f28c49613a10de3216152e8d1ba87
Louis <uqyrq@student.kit.edu>
2025-08-01 17:54:14 +0200

    make vissim system dependent

--- sbaid/model/simulator_factory.py
+++ sbaid/model/simulator_factory.py
@@ -1,5 +1,43 @@
 """This module defines the simulator factory."""
+import sys
+
+from gi.repository import Gio, GObject
+
+from model.simulator.dummy.dummy_simulator import DummySimulator
+from model.simulator.simulator import Simulator
+
+from sbaid.common.simulator_type import SimulatorType
+
+simulator_types = GObject.Property(type=Gio.ListModel,
+                                   flags=GObject.ParamFlags.READABLE |
+                                   GObject.ParamFlags.WRITABLE |
+                                   GObject.ParamFlags.CONSTRUCT_ONLY)
 
 
-class SimulatorFactory:
+class SimulatorException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class InvalidPlatformException(Exception):
+    """TODO"""
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class SimulatorFactory(GObject.GObject):
     """This class defines the simulator factory."""
+    def __init__(self) -> None:
+        # super().__init__(simulator_types=Gio.ListStore(SimulatorType))
+        super().__init__()
+
+    def get_simulator(self, sim_type: SimulatorType) -> Simulator:
+        match sim_type.id:
+            case "dummy_json":
+                return DummySimulator()
+            case "com.ptvgroup.vissim":
+                if sys.platform.startswith("win"):
+                    from model.simulator.vissim.vissim_simulator import VissimSimulator
+                    return VissimSimulator()
+                raise InvalidPlatformException("The Vissim simulator requires Windows.")
+        raise SimulatorException(f"Simulator type {SimulatorType.name} is not supported")
