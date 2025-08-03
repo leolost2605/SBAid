e0d0bd4f8bae3d475fad3e8c1c6251d8a69685fd
Leonhard Kargl <leo.kargl@proton.me>
2025-08-03 23:45:23 +0200

    update context tests

--- tests/model/base/test_context.py
+++ tests/model/base/test_context.py
@@ -1,4 +1,3 @@
-"""TODO"""
 import asyncio
 import unittest
 
@@ -10,11 +9,6 @@ from sbaid.model.context import Context
 
 
 class ContextTestCase(unittest.TestCase):
-
-    def on_delete_async(feld, file, result, user_data):
-        file.delete_finish(result)
-
-
     def test(self):
         self.assertTrue(True)
         asyncio.set_event_loop_policy(GLibEventLoopPolicy())
@@ -25,9 +19,8 @@ class ContextTestCase(unittest.TestCase):
         asyncio.set_event_loop_policy(None)
 
     async def start(self) -> None:
-        # await self.load()
+        await self.load()
         await self.projects()
-
 
     async def load(self):
         global_file = Gio.File.new_for_path("global_db")
@@ -35,35 +28,43 @@ class ContextTestCase(unittest.TestCase):
         context1 = Context()
         await context1.load()
 
-        proj_id = await context1.create_project(SimulatorType("dummy_json",
+        proj_id = await context1.create_project("project name",
+                                                SimulatorType("dummy_json",
                                                               "JSON Dummy Simulator"),
                                                 "simulation_file_path",
                                                 "test_project")
-        # result manager is loaded from the db directly
+
+        self.assertEqual(context1.projects.get_n_items(), 1)
+        self.assertEqual(context1.projects.get_item(0).id, proj_id)
+        self.assertEqual(context1.projects.get_item(0).name, "project name")
+
         context2 = Context()
         await context2.load()
+
         self.assertIsNotNone(context2.projects.get_item(0))
         self.assertEqual(context2.projects.get_item(0).id, proj_id)
-        global_file.delete_async(0, None, self.on_delete_async, None)
+
+        await global_file.delete_async(0, None)
 
     async def projects(self):
         file = Gio.File.new_for_path("global_db")
 
-
         context = Context()
         await context.load()
+
         proj_id_1 = await context.create_project(SimulatorType("dummy_json", "JSON Dummy Simulator"),
                                "simulation_file_path", "test_project")
+
         self.assertEqual(context.projects.get_item(0).id, proj_id_1)
+
         proj_id_2 = await context.create_project(SimulatorType("dummy_json", "JSON Dummy Simulator"),
                                "simulation_file_path", "test_project")
+
         self.assertEqual(context.projects.get_n_items(), 2)
         self.assertNotEqual(context.projects.get_item(0).id, context.projects.get_item(1).id)
 
-
         await context.delete_project(proj_id_1)
         self.assertEqual(context.projects.get_item(0).id, proj_id_2)
+        self.assertEqual(context.projects.get_n_items(), 1)
 
-        file.delete_async(0, None, self.on_delete_async, None)
-
-        self.assertEqual(context.projects.get_item(0).id, proj_id_2)
+        await file.delete_async(0, None)
