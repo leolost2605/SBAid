8863724c165978748efdbc8ec4dd4ef16b6f78b9
Leonhard Kargl <leo.kargl@proton.me>
2025-08-03 23:22:29 +0200

    fix context

--- sbaid/model/context.py
+++ sbaid/model/context.py
@@ -2,6 +2,7 @@
 
 from gi.repository import GObject, Gio, GLib
 
+from sbaid import common
 from sbaid.model.database.global_database import GlobalDatabase
 from sbaid.model.database.global_sqlite import GlobalSQLite
 from sbaid.common.simulator_type import SimulatorType
@@ -9,71 +10,71 @@ from sbaid.model.project import Project
 from sbaid.model.results.result_manager import ResultManager
 
 
+class ProjectNotFoundError(Exception):
+    """Raised when a project couldn't be found."""
+
+
 class Context(GObject.GObject):
     """This class defines the Context class. The root class that is created at startup.
     Manages the projects and holds a reference to the ResultManager."""
 
     __global_db: GlobalDatabase
+    __projects: Gio.ListStore
 
-    __result_id_pos_map: dict[str, int]
+    result_manager: ResultManager = GObject.Property(type=ResultManager,  # type: ignore
+                                                     flags=GObject.ParamFlags.READABLE |
+                                                     GObject.ParamFlags.WRITABLE |
+                                                     GObject.ParamFlags.CONSTRUCT_ONLY)
+    projects: Gio.ListModel = GObject.Property(type=Gio.ListModel,  # type: ignore
+                                               flags=GObject.ParamFlags.READABLE)
 
-    result_manager = GObject.Property(type=ResultManager,
-                                      flags=GObject.ParamFlags.READABLE |
-                                      GObject.ParamFlags.WRITABLE |
-                                      GObject.ParamFlags.CONSTRUCT_ONLY)
-    projects = GObject.Property(type=Gio.ListModel,
-                                flags=GObject.ParamFlags.READABLE |
-                                GObject.ParamFlags.WRITABLE |
-                                GObject.ParamFlags.CONSTRUCT_ONLY)
+    @projects.getter  # type: ignore
+    def projects(self):
+        """Returns the list with all projects"""
+        return self.__projects
 
     def __init__(self) -> None:
-        self.__result_id_pos_map = {}
-        super().__init__(result_manager=ResultManager(),
-                         projects=Gio.ListStore())
+        super().__init__(result_manager=ResultManager())
+        self.__projects = Gio.ListStore.new(Project)
 
     async def load(self) -> None:
         """Loads the projects and the results."""
         self.__global_db = GlobalSQLite(Gio.File.new_for_path("global_db"))
-        db: GlobalDatabase
-        async with self.__global_db as db:
-            projects: list[tuple[str, SimulatorType, str, str]] = \
-                await db.get_all_projects()  # type: ignore
+        await self.__global_db.open()
+
+        projects = await self.__global_db.get_all_projects()
         for project_id, simulator_type, simulation_file_path, project_file_path in projects:
             project = Project(project_id, simulator_type,
                               simulation_file_path, project_file_path, self.result_manager)
-            self.projects.append(project)
-            self.__result_id_pos_map[project_id] = self.projects.find(project)
+            self.__projects.append(project)
             await project.load_from_db()
+
         await self.result_manager.load_from_db()
 
-    async def create_project(self, sim_type: SimulatorType, simulation_file_path: str,
+    async def create_project(self, name: str, sim_type: SimulatorType, simulation_file_path: str,
                              project_file_path: str) -> str:
         """Creates a new project with the given data and returns the unique ID of the new
         project."""
 
         project_id = GLib.uuid_string_random()  # pylint: disable=no-value-for-parameter
 
-        await self.__project_creation_db_action(project_id, sim_type, simulation_file_path,
-                                                project_file_path)
+        await self.__global_db.add_project(project_id, sim_type, simulation_file_path,
+                                           project_file_path)
 
         new_project = Project(project_id, sim_type, simulation_file_path,
                               project_file_path, self.result_manager)
-        self.projects.append(new_project)
-        self.__result_id_pos_map[project_id] = self.projects.find(new_project)[1]
+        self.__projects.append(new_project)
+
+        new_project.name = name
 
         return project_id
 
-    async def __project_creation_db_action(self, project_id: str, sim_type: SimulatorType,
-                                           simulation_file_path: str,
-                                           project_file_path: str) -> None:
-        async with self.__global_db as db:
-            await db.add_project(project_id, sim_type, simulation_file_path, project_file_path)
-
     async def delete_project(self, project_id: str) -> None:
         """Deletes the project with the given ID."""
-        self.projects.remove(self.__result_id_pos_map[project_id])
-        await self.__project_deletion_db_action(project_id)
+        for pos, project in enumerate(common.list_model_iterator(self.__projects)):
+            if project.project_id == project_id:
+                self.__projects.remove(project)
+                await self.__global_db.remove_project(project_id)
+                break
 
-    async def __project_deletion_db_action(self, project_id: str) -> None:
-        async with self.__global_db as db:
-            await db.remove_project(project_id)
+        raise ProjectNotFoundError(f"The Project with the id {project_id} was not found")
