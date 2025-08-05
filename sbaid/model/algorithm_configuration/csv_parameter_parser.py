ca625e0c387ccaa8e85e70f236ccab12b15d5235
leonormsimoes <142237594+leonormsimoes@users.noreply.github.com>
2025-08-05 21:16:35 +0200

    parser works i think

--- sbaid/model/algorithm_configuration/csv_parameter_parser.py
+++ sbaid/model/algorithm_configuration/csv_parameter_parser.py
@@ -2,9 +2,7 @@
 import csv
 import typing
 import aiofiles
-import ast
 from gi.repository import Gio, GLib
-from typing import Any
 
 from sbaid.model.algorithm_configuration.parameter_parser import (
     ParameterParser, ParameterParserForeachFunc)
@@ -12,17 +10,15 @@ from sbaid.model.network.csv_cross_section_parser import InvalidFileFormattingEx
 
 
 class CSVParameterParser(ParameterParser):
-    """This class handles the parsing of CSV files containing global and cross section parameters."""
-
-    __blueprint_types: list[type] = []  # from the parameter configuration
+    """This class handles the parsing of CSV files containing global
+    and cross section parameters."""
 
     def can_handle(self, file_path: str) -> bool:
         """Checks if the given file is a csv file."""
-        # TODO: Make platform independent
         return Gio.content_type_guess(file_path)[0] == ".csv"
 
     async def for_each_parameter(self, file: Gio.File,
-                                 callback: ParameterParserForeachFunc) -> tuple[int, int]:
+                                 foreach_func: ParameterParserForeachFunc) -> tuple[int, int]:
         """Loads the file contents asynchronously and reads the input CSV file row by row,
         attempting to add the parameter to the parameter configuration.
         Returns the amount of added and skipped parameters."""
@@ -31,98 +27,19 @@ class CSVParameterParser(ParameterParser):
         async with aiofiles.open(path, "r+") as csvfile:
             valid_parameters = 0
             invalid_parameters = 0
-            csv_reader = csv.reader(csvfile)
+            csv_reader = csv.reader(await csvfile.readlines())
             try:
-                has_header = self.__has_valid_header(next(csv_reader))
+                header = next(csv_reader)
             except StopIteration as exc:
                 raise InvalidFileFormattingException() from exc
-            if not has_header:
-                await csvfile.seek(0)
+            if header[0].casefold() != "cs_id":
+                raise InvalidFileFormattingException()
             for row in csv_reader:
-                parsed_info = self.__parse_parameter_syntax(row)
-                if parsed_info is not None:
-
-
-
-
-            try:
-                builder = file.read(None)
-                info = builder.read_all(None)[1].decode("utf-8")
-                rows = info.splitlines()
-                csv_reader = csv.reader(rows)
-                header = next(csv_reader)
-                for row in csv_reader:
-
-                    row_info = dict(zip(header, row))
-                    raw_number = row_info.get("QS_NR", '').strip()
-                    cross_section = raw_number
-
-                    for param_name, value in row_info.items():
-                        if param_name == "QS_NR":
-                            continue
-                        value_str = value.strip()
-                        if value_str == '':
-                            continue
-
-                        try:
-                            if value_str == 'True':
-                                variant = GLib.Variant('bool', True)
-                            elif value_str == 'False':
-                                variant = GLib.Variant('bool', False)
-                            elif value_str == 'False':
-                                variant = GLib.Variant('float', float(value_str))
-                            else:
-                                variant = GLib.Variant('int', int(value_str))
-                        except Exception:
-                            raise ValueError("Unsupported value")  #TODO assumir ser um string
-
-                        if not callback(param_name, cross_section, variant):
-                            return valid_parameters, invalid_parameters
-                        valid_parameters += 1
-
-            except Exception:
-                invalid_parameters += 1
-
+                for i in range(len(row)-1):
+                    if row[i+1] != "":
+                        if foreach_func(header[i + 1], row[0],
+                                        GLib.Variant.parse(None, row[i+1])):
+                            valid_parameters += 1
+                        else:
+                            invalid_parameters += 1
         return valid_parameters, invalid_parameters
-
-
-    def __has_valid_header(self, row: list[str]) -> bool:
-        return row[0].casefold() == "cs-name"
-
-    def __compare_types(self, row: list[str]) -> bool:
-        """Compares the found data types in a row with the ones given by the blueprint.
-         Returns a boolean value representing the equality between the types in the
-         blueprint and the given ones."""
-        data_types = self.__find_types(row)
-        for i, data_type in enumerate(data_types):
-            if (data_type != self.__blueprint_types[i]
-                    and data_type != Any):  # if the type is cell was empty
-                return False
-        return True
-
-    def __find_types(self, row: list[str]) -> list[type]:
-        try:
-            param_type = type(ast.literal_eval(row[i]))
-        except ValueError:  # if no byte, int, float, tuple, list, dict, set or None is found
-            param_type = str
-        except SyntaxError:
-            param_type = Any  # TODO fazer depois parameter ter type None
-
-    def __parse_cross_section_params_syntax(self, row: list[str], blueprint: list[type]) -> tuple[bool, bool]:
-        """Analyses a row for correct parameter amount and types. Returns a tuple with two values:
-            - a bool representing whether the parameter row is global
-            - a bool representing the validity of the parameter row."""
-        is_global = row[0] == ""
-        if len(row) != len(blueprint):
-            return False, False
-        valid_counter = 0
-        for i in range(len(row)):
-            try:
-                blueprint[i](row[i])  # cast the row value to blueprint type
-                valid_counter += 1
-            except ValueError:  # allow "" as valid parameter no matter the blueprint type
-                if row[i] != "":
-                    return False
-        if valid_counter == 0:
-            return False  # line is empty; skip
-        return True
--- tests/model/algorithm_configuration/test_csv_param_parser.py
+++ tests/model/algorithm_configuration/test_csv_param_parser.py
@@ -3,13 +3,9 @@ which can be raised during file parsing and handling."""
 import asyncio
 import sys
 import unittest
-from unittest.mock import Mock
-from gi.repository import Gio
-
+from gi.repository import Gio, GLib
 from sbaid.model.algorithm_configuration.csv_parameter_parser import CSVParameterParser
-from sbaid.model.algorithm_configuration.parameter_configuration import ParameterConfiguration
-from sbaid.model.network.network import Network
-from sbaid.model.simulator.dummy.dummy_simulator import DummySimulator
+from sbaid.model.network.csv_cross_section_parser import InvalidFileFormattingException
 
 
 class CsvParameterParserTest(unittest.TestCase):
@@ -19,22 +15,22 @@ class CsvParameterParserTest(unittest.TestCase):
     @unittest.skipUnless(sys.platform.startswith("win"), "Requires Windows")
     def test_file_type_guesser(self):
         parser = CSVParameterParser()
-        self.assertEqual(parser.can_handle_file("valid_parameter_config.csv"), True)
+        self.assertEqual(parser.can_handle("valid_parameter_config.csv"), True)
+
+    def test_valid_parsing(self):
+        asyncio.run(self._testing_callback_func("valid_parameter_config.csv"))
 
     async def _testing_callback_func(self, path: str):
         parser = CSVParameterParser()
         file = Gio.File.new_for_path(path)
         await parser.for_each_parameter(file, self.foreach_func_callback_func)
 
-    async def foreach_func_callback_func(self, name: str) -> bool:
-        network = Network(DummySimulator(), unittest.mock.Mock())
-        parameter_configuration = ParameterConfiguration(network)
-        await parameter_configuration.import_from_file()
+    def foreach_func_callback_func(self, param_name: str, cs_id: str, variant: GLib.Variant) -> bool:
+        print("param name:", param_name, "cs id:", cs_id, "variant:", variant)
+        return True
+
+    def test_invalid_parsing(self):
+        with self.assertRaises(InvalidFileFormattingException):
+            asyncio.run(self._testing_callback_func("invalid_param_import.csv"))
 
 
-    def test_valid_parsing(self):
-        asyncio.run(self._test_valid_parsing())
-
-    async def _test_valid_parsing(self):
-        with self.assertRaises(Exception):
-            await self._testing_callback_func("valid_parameter_config.csv")
