fc123fec161f4edd4c08c65ee7335d46baa3dc45
mariag2804 <mariagavrilita04@gmail.com>
2025-08-08 07:46:40 +0200

    remove todos for simulator observer in base model

--- sbaid/model/simulation_observer.py
+++ sbaid/model/simulation_observer.py
@@ -3,13 +3,17 @@ from gi.repository import GLib, GObject
 
 
 class SimulationObserver(GObject.GObject):
-    """todo"""
+    """This class defines the SimulationObserver interface for observing
+    the status of a simulation"""
 
     def update_progress(self, percentage: float) -> None:
-        """todo"""
+        """Notify the progress of the simulation about the current
+        progress with percentage between 0.0 and 1.0."""
 
     def finished(self, result_id: str) -> None:
-        """todo"""
+        """Notify when the simulation finishes successfully and gives
+        the id of the result that was created in the process"""
 
     def failed(self, error: GLib.Error) -> None:
-        """todo"""
+        """Notify when the simulation failed and passes an error with
+        further information."""
