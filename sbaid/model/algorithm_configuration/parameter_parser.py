49991b7abc4815d732d7f23b0063f8b7018f1b6b
Leonhard Kargl <leo.kargl@proton.me>
2025-08-08 18:03:53 +0200

    fix more todos

--- sbaid/model/algorithm_configuration/parameter_parser.py
+++ sbaid/model/algorithm_configuration/parameter_parser.py
@@ -7,13 +7,25 @@ ParameterParserForeachFunc = Callable[[str, Optional[str], GLib.Variant], bool]
 
 
 class ParameterParser(ABC):
-    """todo"""
+    """
+    The interface for a parameter parser.
+    """
 
     @abstractmethod
     def can_handle_file(self, file_path: str) -> bool:
-        """todo"""
+        """
+        Returns whether this parser implementation can handle the file at the given path.
+        :param file_path: the path of the file to check
+        :return: true if the file can be handled, false otherwise
+        """
 
     @abstractmethod
     async def for_each_parameter(self, file: Gio.File,
                                  foreach_func: ParameterParserForeachFunc) -> Tuple[int, int]:
-        """todo"""
+        """
+        Parses the given file and runs the foreach func for each parameter found in the file.
+        Should only be called after can_handle_file returned true for the file.
+        :param file: the file to parse
+        :param foreach_func: a func to call for each parameter found in the file
+        :return: the number of valid parameters found and the number of invalid parameters
+        """
