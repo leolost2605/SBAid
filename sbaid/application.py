0541fa9b86775c5e5fd5a6aaaa82ce70d0546e84
Louis <uqyrq@student.kit.edu>
2025-08-12 21:18:59 +0200

    remove shutdown method as it doesn't fix the problem

--- sbaid/application.py
+++ sbaid/application.py
@@ -20,10 +20,6 @@ except (ImportError, ValueError) as exc:
     print('Error: Dependencies not met.', exc)
     sys.exit(1)
 
-import tracemalloc
-
-tracemalloc.start()
-
 
 class Application(Adw.Application):
     """
@@ -55,8 +51,3 @@ class Application(Adw.Application):
             self.__window = MainWindow(self.__view_model_context, application=self)
 
         self.__window.present()
-
-    def do_shutdown(self):
-        del self.__window
-        del self.__view_model_context
-        del self
