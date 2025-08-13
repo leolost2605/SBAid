2af75ff45e02e70faf3d13d1a7ab814db74a0b7f
Leonhard Kargl <leo.kargl@proton.me>
2025-08-13 13:00:58 +0200

    Improve welcome view style

--- sbaid/view/start/welcome_page.py
+++ sbaid/view/start/welcome_page.py
@@ -39,6 +39,8 @@ class WelcomePage(Adw.NavigationPage):
         recent_projects_slice = Gtk.SliceListModel.new(sort_model, 0, 3)
 
         self.__last_projects_box = Gtk.ListBox()
+        self.__last_projects_box.add_css_class("background")
+        self.__last_projects_box.set_placeholder(Gtk.Label.new("No recently opened projects"))
         self.__last_projects_box.bind_model(
             recent_projects_slice, self.__create_last_project_button)
 
@@ -51,7 +53,9 @@ class WelcomePage(Adw.NavigationPage):
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6, valign=Gtk.Align.CENTER,
                       halign=Gtk.Align.CENTER)
         box.append(self.__create_project_button)
+        box.append(Gtk.Separator.new(Gtk.Orientation.HORIZONTAL))
         box.append(self.__last_projects_box)
+        box.append(Gtk.Separator.new(Gtk.Orientation.HORIZONTAL))
         box.append(self.__all_projects_button)
         box.append(self.__results_button)
 
@@ -67,9 +71,9 @@ class WelcomePage(Adw.NavigationPage):
     def __sort_func(self, project_one: Project, project_two: Project, data: Any) -> int:
         return project_two.last_modified.compare(project_one.last_modified)
 
-    def __create_last_project_button(self, proj: Project) -> Gtk.Button:
+    def __create_last_project_button(self, proj: Project) -> Gtk.ListBoxRow:
         button = Gtk.Button()
         button.set_action_name("win.open-project")
         button.set_action_target_value(GLib.Variant.new_string(proj.id))
         proj.bind_property("name", button, "label", GObject.BindingFlags.SYNC_CREATE)
-        return button
+        return Gtk.ListBoxRow(child=button, focusable=False)
