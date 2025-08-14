9733953f9b2cf505bfec2732a4d7195ea2adf9fa
Louis <uqyrq@student.kit.edu>
2025-08-15 00:21:19 +0200

    remove make_directory_with_parents_async

--- sbaid/model/database/global_sqlite.py
+++ sbaid/model/database/global_sqlite.py
@@ -7,7 +7,6 @@ import aiopathlib
 from gi.repository import GLib, Gio
 
 from sbaid.model.database.date_format_error import DateFormatError
-from sbaid.common import make_directory_with_parents_async
 from sbaid.common.a_display import ADisplay
 from sbaid.common.b_display import BDisplay
 from sbaid.common.simulator_type import SimulatorType
