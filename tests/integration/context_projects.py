6f321feda4e5b35e71f74a0237a833d6986913f5
Louis <uqyrq@student.kit.edu>
2025-08-27 01:28:26 +0200

    add simulation test (maybe need to rename module and class)

--- tests/integration/context_projects.py
+++ tests/integration/context_projects.py
@@ -1,11 +1,14 @@
 import asyncio
 import unittest
+from typing import cast
 
 from gi.events import GLibEventLoopPolicy
 
 from sbaid.common.simulator_type import SimulatorType
 from sbaid.model.context import Context as ModelContext
+from sbaid.model.project import AlgorithmConfigurationException
 from sbaid.view_model.context import Context
+from sbaid.view_model.project import Project
 
 
 class ContextProjectsTestCase(unittest.TestCase):
@@ -20,7 +23,7 @@ class ContextProjectsTestCase(unittest.TestCase):
     async def start(self) -> None:
         await self.simple()
 
-    async def simple(self):
+    async def simple(self) -> None:
         model_context = ModelContext()
         vm_context = Context(model_context)
 
@@ -45,3 +48,20 @@ class ContextProjectsTestCase(unittest.TestCase):
 
         self.assertEqual(1, len(model_context.projects))
         self.assertEqual(1, len(vm_context.projects))
+
+    async def test_start_simulation(self) -> None:
+        model_context = ModelContext()
+        vm_context = Context(model_context)
+        await vm_context.create_project("project_name_1",
+                                        SimulatorType("dummy_json", "JSON Dummy"),
+                                        "sim_file_path",
+                                        "project_file_path")
+        vm_project = cast(Project, vm_context.projects[0])
+
+        with self.assertRaises(AlgorithmConfigurationException):
+            await vm_project.start_simulation()
+
+        await vm_project.algorithm_configuration_manager.create_algorithm_configuration()
+        vm_project.algorithm_configuration_manager.algorithm_configurations.set_selected(0)
+        await vm_project.start_simulation()
+        self.assertEqual(1, len(vm_context.result_manager.results))
